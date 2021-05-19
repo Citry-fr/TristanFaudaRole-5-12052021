baf206311908d311ba02fdea91488567b9187098
Tristan <citrynox9@gmail.com>
2021-05-19 11:50:33 +0200

    Modification index.js
    Merge 2 function in 1

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -8,40 +8,40 @@ fetch("http://localhost:3000/api/teddies")
     })
     //Utilisation des fonctions qui utilise l'api
     .then(function (prod) {
-        showProduit(prod);
+        // showProduit(prod);
+        displayProducts(prod);
     })
     //Check les erreurs possibles
     .catch(function (error) {
         console.log(error);
     });
+/**
+ * Gère l'affichage des produits
+ * @param {object} produits - Produits
+ */
+function displayProducts(produits) {
+    for (const element of produits) {
+        //Récuperation du template
+        var template = document.querySelector("#prodTemplate");
 
-//Fonction utilisation de l'api
-function showProduit(response) {
-    for (const i in response) {
-        useTemplate(response[i]);
+        //Source et alt pour l'image
+        var img = template.content.querySelector("img");
+        img.src = element.imageUrl;
+        img.alt = "Photo de " + element.name;
+
+        //Nom du produit
+        template.content.querySelector(
+            ".main__article__anchor__card__infos__name"
+        ).textContent = element.name;
+
+        //Ajout de l'id du produit dans l'href du lien
+        template.content.querySelector(".main__article__anchor").href =
+            "./produit.html?id=" + element._id;
+
+        //Ajout du template remplis dans le html
+        var clone = document.importNode(template.content, true);
+        document.getElementById("prodCards").appendChild(clone);
     }
 }
-
-//Fonction remplissage template
-function useTemplate(element) {
-    //Récuperation du template
-    var template = document.querySelector("#prodTemplate");
-
-    //Source et alt pour l'image
-    var img = template.content.querySelector("img");
-    img.src = element.imageUrl;
-    img.alt = "Photo de " + element.name;
-
-    //Nom du produit
-    template.content.querySelector(
-        ".main__article__anchor__card__infos__name"
-    ).textContent = element.name;
-
-    //Ajout de l'id du produit dans l'href du lien
-    template.content.querySelector(".main__article__anchor").href =
-        "./produit.html?id=" + element._id;
-
-    //Ajout du template remplis dans le html
-    var clone = document.importNode(template.content, true);
-    document.getElementById("prodCards").appendChild(clone);
-}
+//test
+console.log(localStorage);
