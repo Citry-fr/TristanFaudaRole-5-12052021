3028d6871ff57ce21dd9a52a778564bb0a1ab663
Tristan <citrynox9@gmail.com>
2021-05-19 18:17:13 +0200

    Started add to cart function

--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -4,6 +4,8 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get("id");
 const urlProd = "http://localhost:3000/api/teddies/" + id;
 
+let produitArray = [];
+
 //Fetch de l'api du produit
 fetch(urlProd)
     //Check de la réponse du serveur
@@ -18,6 +20,7 @@ fetch(urlProd)
         for (const color in produit.colors) {
             fillColor(produit, color);
         }
+        fillCart(produit);
     })
 
     .catch(function (error) {
@@ -55,9 +58,52 @@ function fillColor(element, index) {
     document.getElementById("colorSelect").appendChild(clone);
 }
 
-//test en cours
-const btn = document
-    .getElementById("addCart")
-    .addEventListener("click", function () {
-        console.log("Bouton clické !");
-    });
+string = (input) => {
+    localStorage.setItem("produits", JSON.stringify(input));
+};
+
+function fillCart(element) {
+    const btn = document
+        .getElementById("addCart")
+        .addEventListener("click", function () {
+            const parse = JSON.parse(localStorage.getItem("produits"));
+            const select = document.getElementById("colorSelect");
+
+            let produitModif;
+            let produit = {
+                prodName: element.name,
+                prodPrice: element.price,
+                prodId: element._id,
+                prodColor: document.getElementById("colorSelect").value,
+                prodQuantity: 1,
+            };
+
+            if (localStorage.getItem("produits") === null) {
+                produitArray.push(produit);
+                localStorage.setItem("produits", JSON.stringify(produitArray));
+                console.log(localStorage);
+            } else {
+                for (const prod in parse) {
+                    if (
+                        produit.prodId === parse[prod].prodId &&
+                        produit.prodColor === parse[prod].prodColor
+                    ) {
+                        produitModif = parse[prod];
+                        console.log(produitModif);
+                        produitModif.prodQuantity++;
+                        string(produitModif);
+                        console.log("Incrémente");
+                        break;
+                    } else {
+                        produitArray.push(produit);
+                        string(produitArray);
+                        console.log("ADD");
+                    }
+                    console.log(parse[prod]);
+                }
+
+                console.log(localStorage);
+            }
+            console.log(localStorage.getItem("produits"));
+        });
+}
