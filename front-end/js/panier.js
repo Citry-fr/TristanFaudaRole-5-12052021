185fd1ea3d46a86fd11ffe991219fc4976540409
Tristan <citrynox9@gmail.com>
2021-05-21 16:05:28 +0200

    Cart table list localstorage product

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -10,3 +10,20 @@ function cartAmount() {
 cartAmount();
 
 let productArray = JSON.parse(localStorage.getItem("produits"));
+
+for (const product in productArray) {
+    useTemplate(productArray[product]);
+}
+
+function useTemplate(input) {
+    var template = document.querySelector("#cartTemplate");
+
+    var td = template.content.querySelectorAll(".main__table__body__row__cell");
+    td[0].textContent = input.prodName;
+    td[1].textContent = input.prodColor;
+    td[2].textContent = input.prodQuantity;
+    td[3].textContent = input.prodPrice;
+
+    var clone = document.importNode(template.content, true);
+    document.getElementById("tableBody").appendChild(clone);
+}
