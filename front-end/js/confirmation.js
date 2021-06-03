a38e4df871ae7fcf58d0e1bef140c759c0d2439a
Tristan <citrynox9@gmail.com>
2021-06-03 16:04:01 +0200

    Confirmation style and LocalStorage delete

--- front-end/js/confirmation.js
+++ front-end/js/confirmation.js
@@ -1,6 +1,24 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const price = urlParams.get("price");
+const urlInfos = {
+    orderId: urlParams.get("orderid"),
+    orderPrice: urlParams.get("price"),
+    clientFirstName: urlParams.get("fname"),
+    clientLastName: urlParams.get("lname"),
+};
 
-console.log(price);
-console.log(urlParams.get("orderid"));
+const htmlElement = {
+    thankingTitle: document.getElementById("thanking"),
+    orderIdSpan: document.getElementById("orderID"),
+    orderPriceSpan: document.getElementById("orderPrice"),
+};
+
+function deleteLocalStorage() {
+    localStorage.removeItem("produits");
+}
+
+htmlElement.thankingTitle.textContent = `Mr/Mme ${urlInfos.clientLastName} ${urlInfos.clientFirstName}, merci pour votre commande sur Orinoco.`;
+htmlElement.orderIdSpan.textContent = urlInfos.orderId;
+htmlElement.orderPriceSpan.textContent = urlInfos.orderPrice;
+
+deleteLocalStorage();
